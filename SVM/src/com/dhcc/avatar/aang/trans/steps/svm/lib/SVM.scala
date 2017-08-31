66572014e1205c636313a335722bd7c655822374
yanqinghao <187280967@qq.com>
2017-08-31 18:01:34 +0800

    finish

--- SVM/src/com/dhcc/avatar/aang/trans/steps/svm/lib/SVM.scala
+++ SVM/src/com/dhcc/avatar/aang/trans/steps/svm/lib/SVM.scala
@@ -16,8 +16,13 @@ import org.apache.spark.mllib.classification.SVMWithSGD
 import org.apache.spark.mllib.classification.SVMModel
 import org.apache.spark.sql.DataFrame
 import java.io.File
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.DataFrame
+import org.dmg.pmml.True
 
 class SVM(@transient val sc: SparkContext, val numIterations: Int) extends Serializable {
+
   val sqlContext = new SQLContext(sc)
 
   private def deleteDir(dir: File): Boolean = {
@@ -66,19 +71,28 @@ class SVM(@transient val sc: SparkContext, val numIterations: Int) extends Seria
     model
   }
 
-  def svmPredict(model: SVMModel, preData: DataFrame): DataFrame = {
-    val col = preData.columns
-    val size = col.length
-    val rddArr = preData.rdd.map(toArr(_, size))
-    val parsedData = rddArr.map(r => LabeledPoint(r(size - 1), Vectors.dense(toVec(r, size))))
-    val index = model.predict(parsedData.map(_.features))
-    val predictionAndLabel = index.zip(parsedData.map(_.label))
-    val result = sqlContext.createDataFrame(predictionAndLabel).toDF("pre", "real")
-    //    val schemaString = "label"
-    //    val schema = StructType(schemaString.split(" ").map(a => StructField(a, IntegerType, true)))
-    //    val rowRDD = index.map(Row(_))
-    //    val predict = sqlContext.createDataFrame(rowRDD, schema)
-    result
+  def svmPredict(model: SVMModel, preData: DataFrame, withLabel: Boolean): DataFrame = {
+    val res = new Array[DataFrame](1)
+    if (withLabel) {
+      val col = preData.columns
+      val size = col.length
+      val rddArr = preData.rdd.map(toArr(_, size))
+      val parsedData = rddArr.map(r => LabeledPoint(r(size - 1), Vectors.dense(toVec(r, size))))
+      val index = model.predict(parsedData.map(_.features))
+      val predictionAndLabel = index.zip(parsedData.map(_.label))
+      val result = sqlContext.createDataFrame(predictionAndLabel).toDF("pre", "real")
+      res(0) = result
+    } else {
+      val col = preData.columns
+      val size = col.length
+      val rddArr = preData.rdd.map(toArr(_, size))
+      val parsedData = rddArr.map(r => Vectors.dense(r))
+      val index = model.predict(parsedData)
+      val pre = index.map(r => Tuple1(r))
+      val result = sqlContext.createDataFrame(pre).toDF("pre")
+      res(0) = result
+    }
+    res(0)
   }
 
 }
@@ -88,30 +102,40 @@ object SVM {
     val conf = new SparkConf().setAppName("SVM").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
-    val path = "E:\\桌面\\svmdata\\train.csv"
-    val path1 = "E:\\桌面\\svmdata\\predict.csv"
-    val fileData = sc.textFile(path)
-    val rddRow = fileData.map(_.split(",")).map(f => Row(f(0).toDouble, f(1).toDouble, f(2).toDouble))
+    val trainpath = "E:\\桌面\\svmdata\\train.csv"
+    val prepathlab = "E:\\桌面\\svmdata\\predict.csv"
+    val prepath = "E:\\桌面\\svmdata\\predict1.csv"
+    val trainData = sc.textFile(trainpath)
+    val rddRow = trainData.map(_.split(",")).map(f => Row(f(0).toDouble, f(1).toDouble, f(2).toDouble))
     val schemaString = "c1 c2 label"
     val schema = StructType(schemaString.split(" ").map(a => StructField(a, DoubleType, true)))
-    val fileDataFrame = sqlContext.createDataFrame(rddRow, schema)
-    //val training = fileDataFrame.rdd.map(r => LabeledPoint(r.getAs[Double]("label"), Vectors.dense(r.get(0).asInstanceOf[Double], r.get(1).asInstanceOf[Double])))
+    val trainDF = sqlContext.createDataFrame(rddRow, schema)
 
-    val fileData1 = sc.textFile(path1)
-    val rddRow1 = fileData1.map(_.split(",")).map(f => Row(f(0).toDouble, f(1).toDouble, f(2).toDouble))
+    val preDatalab = sc.textFile(prepathlab)
+    val rddRow1 = preDatalab.map(_.split(",")).map(f => Row(f(0).toDouble, f(1).toDouble, f(2).toDouble))
     val schemaString1 = "c1 c2 label"
     val schema1 = StructType(schemaString1.split(" ").map(a => StructField(a, DoubleType, true)))
-    val fileDataFrame1 = sqlContext.createDataFrame(rddRow1, schema1)
-    //val testing = fileDataFrame1.rdd.map(r => LabeledPoint(r.getAs[Double]("label"), Vectors.dense(r.get(0).asInstanceOf[Double], r.get(1).asInstanceOf[Double])))
-    //val testing = training
+    val preDFlab = sqlContext.createDataFrame(rddRow1, schema1)
+
+    val preData = sc.textFile(prepath)
+    val rddRow2 = preData.map(_.split(",")).map(f => Row(f(0).toDouble, f(1).toDouble))
+    val schemaString2 = "c1 c2"
+    val schema2 = StructType(schemaString2.split(" ").map(a => StructField(a, DoubleType, true)))
+    val preDF = sqlContext.createDataFrame(rddRow2, schema2)
+
+    val withLabel = true
     val numIterations = 1000
     val modelPath = "E://svmMdl//"
     val svmIns = new SVM(sc, numIterations)
-    val model = svmIns.svmTrain(fileDataFrame, modelPath)
-    val result = svmIns.svmPredict(model, fileDataFrame1)
+    val model = svmIns.svmTrain(trainDF, modelPath)
+    val result = svmIns.svmPredict(model, preDFlab, withLabel)
+    val result2 = svmIns.svmPredict(model, preDF, false)
     val model1 = svmIns.svmLoad(modelPath)
-    val result1 = svmIns.svmPredict(model1, fileDataFrame1)
+    val result1 = svmIns.svmPredict(model1, preDFlab, withLabel)
+    val result3 = svmIns.svmPredict(model1, preDF, false)
     result.show()
     result1.show()
+    result2.show()
+    result3.show()
   }
 }
